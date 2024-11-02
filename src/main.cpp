07f3dd10316a2dc2f48f5d37f7066d73cb035d7b
Akbar Sena Wijaya <120768034+akbarsenawjy26@users.noreply.github.com>
2024-11-02 13:07:53 +0700

    feat(main): membuat main program v0.1.0

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,32 @@
 #include <Arduino.h>
+#include <AntaresESPMQTT.h>
 
-// put function declarations here:
-int myFunction(int, int);
+#define ACCESSKEY "0d821832d7891950:c67abf7582e43b62"
+#define WIFISSID "oke makasi"
+#define PASSWORD "kaliporong84"
+#define projectName "antares-monitoring" // Name of the application created in Antares
+#define deviceName "node-1"
 
-void setup() {
+AntaresESPMQTT antares(ACCESSKEY);
+
+void setup()
+{
   // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(115200);
+  antares.setDebug(true);
+  antares.wifiConnection(WIFISSID, PASSWORD);
+  antares.setMqttServer();
 }
 
-void loop() {
+void loop()
+{
   // put your main code here, to run repeatedly:
-}
+  antares.checkMqttConnection();
+  int temp = random(25, 30);
+  int hum = random(75, 90);
+  antares.add("temperature", temp);
+  antares.add("humidity", hum);
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  antares.publish(projectName, deviceName);
+  delay(5000);
 }
